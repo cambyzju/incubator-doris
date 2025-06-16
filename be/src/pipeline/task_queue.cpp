01b026a86a8aac5c227bd4f00ae178e8e58f29aa
cambyzhu <cambyzhu@tencent.com>
2025-06-16 15:33:57 +0800

    add metrics pipeline_task_queue_size

--- be/src/pipeline/task_queue.cpp
+++ be/src/pipeline/task_queue.cpp
@@ -78,6 +78,7 @@ PipelineTask* PriorityTaskQueue::_try_take_unprotected(bool is_steal) {
     if (task) {
         task->update_queue_level(level);
         _total_task_size--;
+        DorisMetrics::instance()->pipeline_task_queue_size->increment(-1);
     }
     return task;
 }
@@ -127,6 +128,7 @@ Status PriorityTaskQueue::push(PipelineTask* task) {
 
     _sub_queues[level].push_back(task);
     _total_task_size++;
+    DorisMetrics::instance()->pipeline_task_queue_size->increment(1);
     _wait_task.notify_one();
     return Status::OK();
 }
