4ef60e9bb636c92f4eb3602b33de0a5c82ed868f
cambyzhu <cambyzhu@tencent.com>
2025-12-18 16:08:00 +0800

    support more hive data types

--- fe/be-java-extensions/jdbc-scanner/src/main/java/org/apache/doris/jdbc/Hive2JdbcExecutor.java
+++ fe/be-java-extensions/jdbc-scanner/src/main/java/org/apache/doris/jdbc/Hive2JdbcExecutor.java
@@ -22,17 +22,10 @@ import org.apache.doris.common.jni.vec.ColumnType.Type;
 import org.apache.doris.common.jni.vec.ColumnValueConverter;
 import org.apache.doris.common.jni.vec.VectorTable;
 
-import com.google.common.collect.Lists;
-
-import java.lang.reflect.Array;
 import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.net.InetAddress;
+import java.sql.Date;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Timestamp;
 
 public class Hive2JdbcExecutor extends BaseJdbcExecutor {
 
@@ -56,38 +49,45 @@ public class Hive2JdbcExecutor extends BaseJdbcExecutor {
     protected Object getColumnValue(int columnIndex, ColumnType type, String[] replaceStringList) throws SQLException {
         switch (type.getType()) {
             case BOOLEAN:
-                return resultSet.getObject(columnIndex + 1, Boolean.class);
+                boolean boolVal = resultSet.getBoolean(columnIndex + 1);
+                return resultSet.wasNull() ? null : boolVal;
             case TINYINT:
-                return resultSet.getObject(columnIndex + 1, Byte.class);
+                byte tinyIntVal = resultSet.getByte(columnIndex + 1);
+                return resultSet.wasNull() ? null : tinyIntVal;
             case SMALLINT:
-                return resultSet.getObject(columnIndex + 1, Short.class);
+                short smallIntVal = resultSet.getShort(columnIndex + 1);
+                return resultSet.wasNull() ? null : smallIntVal;
             case INT:
-                return resultSet.getObject(columnIndex + 1, Integer.class);
+                int intVal = resultSet.getInt(columnIndex + 1);
+                return resultSet.wasNull() ? null : intVal;
             case BIGINT:
-                return resultSet.getObject(columnIndex + 1, Long.class);
-            case LARGEINT:
-                return resultSet.getObject(columnIndex + 1, BigInteger.class);
+                long bigIntVal = resultSet.getLong(columnIndex + 1);
+                return resultSet.wasNull() ? null : bigIntVal;
             case FLOAT:
-                return resultSet.getObject(columnIndex + 1, Float.class);
+                float floatVal = resultSet.getFloat(columnIndex + 1);
+                return resultSet.wasNull() ? null : floatVal;
             case DOUBLE:
-                return resultSet.getObject(columnIndex + 1, Double.class);
+                double doubleVal = resultSet.getDouble(columnIndex + 1);
+                return resultSet.wasNull() ? null : doubleVal;
             case DECIMALV2:
             case DECIMAL32:
             case DECIMAL64:
             case DECIMAL128:
-                return resultSet.getObject(columnIndex + 1, BigDecimal.class);
+                BigDecimal decimalVal = resultSet.getBigDecimal(columnIndex + 1);
+                return resultSet.wasNull() ? null : decimalVal;
             case DATE:
             case DATEV2:
-                return resultSet.getObject(columnIndex + 1, LocalDate.class);
+                Date dateVal = resultSet.getDate(columnIndex + 1);
+                return resultSet.wasNull() ? null : dateVal.toLocalDate();
             case DATETIME:
             case DATETIMEV2:
-                return resultSet.getObject(columnIndex + 1, LocalDateTime.class);
+                Timestamp timestampVal = resultSet.getTimestamp(columnIndex + 1);
+                return resultSet.wasNull() ? null : timestampVal.toLocalDateTime();
             case CHAR:
             case VARCHAR:
             case STRING:
-                return resultSet.getObject(columnIndex + 1, String.class);
-            case ARRAY:
-                return convertArrayToList(resultSet.getArray(columnIndex + 1).getArray());
+                String stringVal = resultSet.getString(columnIndex + 1);
+                return resultSet.wasNull() ? null : stringVal;
             default:
                 throw new IllegalArgumentException("Unsupported column type: " + type.getType());
         }
@@ -95,130 +95,6 @@ public class Hive2JdbcExecutor extends BaseJdbcExecutor {
 
     @Override
     protected ColumnValueConverter getOutputConverter(ColumnType columnType, String replaceString) {
-        if (columnType.getType() == Type.ARRAY) {
-            return createConverter(
-                    (Object input) -> convertArray((List<?>) input, columnType.getChildTypes().get(0)),
-                    List.class);
-        } else {
-            return null;
-        }
-    }
-
-    private List<Object> convertArrayToList(Object array) {
-        if (array == null) {
-            return null;
-        }
-
-        int length = Array.getLength(array);
-        List<Object> list = new ArrayList<>(length);
-
-        for (int i = 0; i < length; i++) {
-            Object element = Array.get(array, i);
-            list.add(element);
-        }
-
-        return list;
-    }
-
-    private List<?> convertArray(List<?> array, ColumnType type) {
-        if (array == null) {
-            return null;
-        }
-        switch (type.getType()) {
-            case SMALLINT: {
-                List<Short> result = Lists.newArrayList();
-                for (Object element : array) {
-                    if (element == null) {
-                        result.add(null);
-                    } else {
-                        if (element instanceof Byte) {
-                            result.add(((Byte) element).shortValue());
-                        } else if (element instanceof Number) {
-                            result.add(((Number) element).shortValue());
-                        } else {
-                            throw new IllegalArgumentException("Unsupported element type: " + element.getClass());
-                        }
-                    }
-                }
-                return result;
-            }
-            case INT: {
-                List<Integer> result = Lists.newArrayList();
-                for (Object element : array) {
-                    if (element == null) {
-                        result.add(null);
-                    } else {
-                        if (element instanceof Short) {
-                            result.add(((Short) element).intValue());
-                        } else if (element instanceof Number) {
-                            result.add(((Number) element).intValue());
-                        } else {
-                            throw new IllegalArgumentException("Unsupported element type: " + element.getClass());
-                        }
-                    }
-                }
-                return result;
-            }
-            case BIGINT: {
-                List<Long> result = Lists.newArrayList();
-                for (Object element : array) {
-                    if (element == null) {
-                        result.add(null);
-                    } else {
-                        if (element instanceof Integer) {
-                            result.add(((Integer) element).longValue());
-                        } else if (element instanceof Number) {
-                            result.add(((Number) element).longValue());
-                        } else {
-                            throw new IllegalArgumentException("Unsupported element type: " + element.getClass());
-                        }
-                    }
-                }
-                return result;
-            }
-            case LARGEINT: {
-                List<BigInteger> result = Lists.newArrayList();
-                for (Object element : array) {
-                    if (element == null) {
-                        result.add(null);
-                    } else {
-                        if (element instanceof BigDecimal) {
-                            result.add(((BigDecimal) element).toBigInteger());
-                        } else if (element instanceof Number) {
-                            result.add(BigInteger.valueOf(((Number) element).longValue()));
-                        } else {
-                            throw new IllegalArgumentException("Unsupported element type: " + element.getClass());
-                        }
-                    }
-                }
-                return result;
-            }
-            case STRING: {
-                List<String> result = Lists.newArrayList();
-                for (Object element : array) {
-                    if (element == null) {
-                        result.add(null);
-                    } else if (element instanceof InetAddress) {
-                        result.add(((InetAddress) element).getHostAddress());
-                    } else {
-                        result.add(element.toString());
-                    }
-                }
-                return result;
-            }
-            case ARRAY:
-                List<List<?>> resultArray = Lists.newArrayList();
-                for (Object element : array) {
-                    if (element == null) {
-                        resultArray.add(null);
-                    } else {
-                        resultArray.add(
-                                Lists.newArrayList(convertArray((List<?>) element, type.getChildTypes().get(0))));
-                    }
-                }
-                return resultArray;
-            default:
-                return array;
-        }
+        return null;
     }
 }
--- fe/fe-core/src/main/java/org/apache/doris/datasource/jdbc/client/JdbcHive2Client.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/jdbc/client/JdbcHive2Client.java
@@ -17,20 +17,12 @@
 
 package org.apache.doris.datasource.jdbc.client;
 
-import org.apache.doris.catalog.ArrayType;
 import org.apache.doris.catalog.ScalarType;
 import org.apache.doris.catalog.Type;
-import org.apache.doris.common.util.Util;
 import org.apache.doris.datasource.jdbc.util.JdbcFieldSchema;
 
-import com.google.common.collect.Maps;
-
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Map;
-import java.util.Optional;
 
 public class JdbcHive2Client extends JdbcClient {
     protected JdbcHive2Client(JdbcClientConfig jdbcClientConfig) {
@@ -54,49 +46,14 @@ public class JdbcHive2Client extends JdbcClient {
 
     @Override
     protected Type jdbcTypeToDoris(JdbcFieldSchema fieldSchema) {
-        // For mysql type: "INT UNSIGNED":
-        // fieldSchema.getDataTypeName().orElse("unknown").split(" ")[0] == "INT"
-        // fieldSchema.getDataTypeName().orElse("unknown").split(" ")[1] == "UNSIGNED"
-        String[] typeFields = fieldSchema.getDataTypeName().orElse("unknown").split(" ");
-        String mysqlType = typeFields[0];
-        // For unsigned int, should extend the type.
-        if (typeFields.length > 1 && "UNSIGNED".equals(typeFields[1])) {
-            switch (mysqlType) {
-                case "TINYINT":
-                    return Type.SMALLINT;
-                case "SMALLINT":
-                case "MEDIUMINT":
-                    return Type.INT;
-                case "INT":
-                    return Type.BIGINT;
-                case "BIGINT":
-                    return Type.LARGEINT;
-                case "DECIMAL": {
-                    int precision = fieldSchema.requiredColumnSize() + 1;
-                    int scale = fieldSchema.requiredDecimalDigits();
-                    return createDecimalOrStringType(precision, scale);
-                }
-                case "DOUBLE":
-                    // As of MySQL 8.0.17, the UNSIGNED attribute is deprecated
-                    // for columns of type FLOAT, DOUBLE, and DECIMAL (and any synonyms)
-                    // https://dev.mysql.com/doc/refman/8.0/en/numeric-type-syntax.html
-                    // The maximum value may cause errors due to insufficient accuracy
-                    return Type.DOUBLE;
-                case "FLOAT":
-                    return Type.FLOAT;
-                default:
-                    throw new JdbcClientException("Unknown UNSIGNED type of mysql, type: [" + mysqlType + "]");
-            }
-        }
-        switch (mysqlType) {
+        String hiveType = fieldSchema.getDataTypeName().orElse("unknown");
+        switch (hiveType) {
             case "BOOLEAN":
                 return Type.BOOLEAN;
             case "TINYINT":
                 return Type.TINYINT;
             case "SMALLINT":
-            case "YEAR":
                 return Type.SMALLINT;
-            case "MEDIUMINT":
             case "INT":
                 return Type.INT;
             case "BIGINT":
@@ -105,10 +62,8 @@ public class JdbcHive2Client extends JdbcClient {
                 return ScalarType.createDateV2Type();
             case "TIMESTAMP":
             case "DATETIME": {
-                // mysql can support microsecond
-                // use columnSize to calculate the precision of timestamp/datetime
-                int columnSize = fieldSchema.requiredColumnSize();
-                int scale = columnSize > 19 ? columnSize - 20 : 0;
+                // hive can support microsecond
+                int scale = fieldSchema.getDecimalDigits().orElse(0);
                 if (scale > 6) {
                     scale = 6;
                 }
@@ -127,136 +82,9 @@ public class JdbcHive2Client extends JdbcClient {
                 return ScalarType.createCharType(fieldSchema.requiredColumnSize());
             case "VARCHAR":
                 return ScalarType.createVarcharType(fieldSchema.requiredColumnSize());
-            case "BIT":
-                if (fieldSchema.requiredColumnSize() == 1) {
-                    return Type.BOOLEAN;
-                } else {
-                    return ScalarType.createStringType();
-                }
-            case "JSON":
-            case "TIME":
-            case "TINYTEXT":
-            case "TEXT":
-            case "MEDIUMTEXT":
-            case "LONGTEXT":
-            case "TINYBLOB":
-            case "BLOB":
-            case "MEDIUMBLOB":
-            case "LONGBLOB":
             case "STRING":
-            case "SET":
             case "BINARY":
-            case "VARBINARY":
-            case "ENUM":
                 return ScalarType.createStringType();
-            default:
-                return Type.UNSUPPORTED;
-        }
-    }
-
-    /**
-     * get all columns like DatabaseMetaData.getColumns in mysql-jdbc-connector
-     */
-    private Map<String, String> getColumnsDataTypeUseQuery(String remoteDbName, String remoteTableName) {
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet resultSet = null;
-        Map<String, String> fieldToType = Maps.newHashMap();
-
-        StringBuilder queryBuf = new StringBuilder("SHOW FULL COLUMNS FROM ");
-        queryBuf.append("`").append(remoteTableName).append("`");
-        queryBuf.append(" FROM ");
-        queryBuf.append("`").append(remoteDbName).append("`");
-        try {
-            conn = getConnection();
-            stmt = conn.createStatement();
-            resultSet = stmt.executeQuery(queryBuf.toString());
-            while (resultSet.next()) {
-                // get column name
-                String fieldName = resultSet.getString("Field");
-                // get original type name
-                String typeName = resultSet.getString("Type");
-                fieldToType.put(fieldName, typeName);
-            }
-        } catch (SQLException e) {
-            throw new JdbcClientException("failed to get jdbc columns info for remote table `%s.%s`: %s",
-                    remoteDbName, remoteTableName, Util.getRootCauseMessage(e));
-        } finally {
-            close(resultSet, stmt, conn);
-        }
-        return fieldToType;
-    }
-
-    private Type dorisTypeToDoris(JdbcFieldSchema fieldSchema) {
-        String type = fieldSchema.getDataTypeName().orElse("unknown").toUpperCase();
-        if (type == null || type.isEmpty()) {
-            return Type.UNSUPPORTED;
-        }
-
-        String upperType = type.toUpperCase();
-
-        // For ARRAY type
-        if (upperType.startsWith("ARRAY")) {
-            String innerType = upperType.substring(6, upperType.length() - 1).trim();
-            JdbcFieldSchema innerFieldSchema = new JdbcFieldSchema(fieldSchema);
-            innerFieldSchema.setDataTypeName(Optional.of(innerType));
-            Type arrayInnerType = dorisTypeToDoris(innerFieldSchema);
-            return ArrayType.create(arrayInnerType, true);
-        }
-
-        int openParen = upperType.indexOf("(");
-        String baseType = (openParen == -1) ? upperType : upperType.substring(0, openParen);
-
-        switch (baseType) {
-            case "BOOL":
-            case "BOOLEAN":
-                return Type.BOOLEAN;
-            case "TINYINT":
-                return Type.TINYINT;
-            case "INT":
-                return Type.INT;
-            case "SMALLINT":
-                return Type.SMALLINT;
-            case "BIGINT":
-                return Type.BIGINT;
-            case "LARGEINT":
-                return Type.LARGEINT;
-            case "FLOAT":
-                return Type.FLOAT;
-            case "DOUBLE":
-                return Type.DOUBLE;
-            case "DECIMAL":
-            case "DECIMALV3": {
-                int precision = fieldSchema.requiredColumnSize();
-                int scale = fieldSchema.requiredDecimalDigits();
-                return createDecimalOrStringType(precision, scale);
-            }
-            case "DATE":
-            case "DATEV2":
-                return ScalarType.createDateV2Type();
-            case "DATETIME":
-            case "DATETIMEV2": {
-                int scale = (openParen == -1) ? 6
-                        : Integer.parseInt(upperType.substring(openParen + 1, upperType.length() - 1));
-                if (scale > 6) {
-                    scale = 6;
-                }
-                return ScalarType.createDatetimeV2Type(scale);
-            }
-            case "CHAR":
-            case "CHARACTER":
-                return ScalarType.createCharType(fieldSchema.requiredColumnSize());
-            case "VARCHAR":
-                return ScalarType.createVarcharType(fieldSchema.requiredColumnSize());
-            case "STRING":
-            case "TEXT":
-            case "JSON":
-            case "JSONB":
-                return ScalarType.createStringType();
-            case "HLL":
-                return ScalarType.createHllType();
-            case "BITMAP":
-                return Type.BITMAP;
             default:
                 return Type.UNSUPPORTED;
         }
